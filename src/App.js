6716bfa81d9cc1b1bc47988a2f95d9798454df76
Shaikh Noman <noman.shaikh@neosoftmail.com>
2023-02-27 12:40:50 +0530

    Backup

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from "react";
 import { Header } from "./components/header/Header";
 import "./App.css";
+import { SideBar } from "./components/sidebar/SideBar";
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <div className="app_body">
+        <SideBar />
+      </div>
     </div>
   );
 }
---
+++ src/components/sidebar/SideBar.js
@@ -0,0 +1,32 @@
+import React from "react";
+import "./Sidebar.css";
+import { Avatar } from "@mui/material";
+
+export const SideBar = () => {
+  return (
+    <div className="sidebar">
+      <div className="sidebar_top">
+        <img
+          src="https://images.unsplash.com/photo-1675806963317-6fbf3b6d61ab?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHx0b3BpYy1mZWVkfDN8Ym84alFLVGFFMFl8fGVufDB8fHx8&auto=format&fit=crop&w=800&q=60"
+          alt=""
+        />
+        <Avatar className="sidebar_avatar" />
+        <h2>Shaikh Noman</h2>
+        <h4>noman@gmail.com </h4>
+      </div>
+      <div className="side_stats">
+        <div className="side_bar_stat">
+          <p>Who viewed you </p>
+          <p className="side_bar_statNumber">2,500</p>
+        </div>
+        <div className="side_bar_stat">
+          <p>Views on Post</p>
+          <p className="side_bar_statNumber">3,347</p>
+        </div>
+      </div>
+      <div className="sidebar_button">
+        <p>Recent</p>
+      </div>
+    </div>
+  );
+};
